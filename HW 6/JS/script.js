0a2ba50667efd4a260dfc80d06b1167eab72ebc2
EzhiChernyavski <yura.chernyavski.97@mail.ru>
2020-02-22 11:41:09 +0300

    4-6

--- HW 6/JS/script.js
+++ HW 6/JS/script.js
@@ -22,5 +22,53 @@ console.log(str2.substr(6, 10));
 console.log(str2.slice(2, 6));
 
 //№4
-let arr = [4, 2, 5, 19, 13, 0, 10];
+let arr = [4, 2, 5, 19, 13, 0, 10],
+    sum = 0
 
+console.log(arr);
+
+for(let i = 0; i < arr.length; i++) {
+    sum += Math.pow(arr[i],3)
+    console.log(arr[i]);
+};
+
+let sqrArr = Math.sqrt(sum);
+
+console.log(sqrArr);
+
+//№5
+let a = 3,
+    b = 5,
+    c = a - b;
+
+    console.log(Math.abs(c));
+
+
+//№6
+dateNow = new Date();
+//console.log(dateNow);
+
+function fomatDate(date) {
+    let dd = date.getDate();
+    if (dd < 10) {
+        dd += '0'
+    };
+
+    let mm = month.getDate();
+    if (mm < 10) {
+        mm += '0'
+    };
+};
+
+/*let options = {
+    month: 'numeric'
+}*/
+
+console.log(dateNow.toLocaleString('ru', /*options*/));
+
+//№7
+/*let phone = prompt('Введиье телефон в международном формате')
+
+function checks(phone) {
+    if ( phone = //)
+};*/
