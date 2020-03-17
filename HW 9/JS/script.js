f2ee1f0fe383136d83f21bd96e43b111eddd27d2
EzhiChernyavski <yura.chernyavski.97@mail.ru>
2020-03-17 23:32:21 +0300

    ДЗ 9 не готовое

--- HW 9/JS/script.js
+++ HW 9/JS/script.js
@@ -1,16 +1,65 @@
 //Напишите ф-цию отправки различных уведомлений, например: уведомл. об успешной отправки формы, приветственное сообщение или сообщение об ошибке. Ф-ция должна приниматься сл. параметры: тип сообщения (type: error, alert, msg), текст сообщения (string). После вызова ф-ции в HTML- документе должно появится сообщение (<div>) соответствующего типа, и спустя 5 секунд должно пропадать. Каждый тип сообщения должен отличатся по стилю, например: фоновым цветом и шрифтом.
 
 
+let notification = function(type, string) {
 
-let div = document.createElement('div')
-let text = document.createTextNode('Форма успешно отправлена');
+    let alert = document.createElement('div');
+    string = document.createTextNode('Сообщение успешно     отправлено');
+
+    alert.className = 'alert';
+
+    alert.appendChild(string);
+    document.body.appendChild(alert);
+
+    let msg = document.createElement('div');
+    let str = document.createTextNode('Добро пожаловать!');
+
+    msg.className = 'msg';
+
+    msg.appendChild(str);
+    document.body.appendChild(msg);
+
+    let error = document.createElement('div');
+    let st = document.createTextNode('Сообщение не отправлено!');
+
+    error.className = 'st';
+
+    error.appendChild(st);
+    document.body.appendChild(error);
+
+    if (type = alert) {
+        string = document.createTextNode('Сообщение успешно     отправлено');
+    } if (type = msg) {
+        string = document.createTextNode('Добро пожаловать!');
+    } if (type = error) {
+        string = document.createTextNode('Сообщение не отправлено!');
+    } 
+};
 
 
-div.className = 'alert success';
-div.appendChild(text);
-document.body.appendChild(div);
 
-div.innerHTML = '<div><p>Приветствуем на нашем сайте</p></div>'
+/*let alert = document.createElement('div');
+let string = document.createTextNode('Сообщение успешно отправлено');
 
+alert.className = 'alert';
+
+alert.appendChild(string);
+document.body.appendChild(alert);
+
+let msg = document.createElement('div');
+let str = document.createTextNode('Добро пожаловать!');
+
+msg.className = 'msg';
+
+msg.appendChild(str);
+document.body.appendChild(msg);
+
+let error = document.createElement('div');
+let st = document.createTextNode('Сообщение не отправлено!');
+
+error.className = 'st';
+
+error.appendChild(st);
+document.body.appendChild(error);*/
 
 
